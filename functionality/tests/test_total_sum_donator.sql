07a24b4de7c9a76cbcad009124486a0b93e536f9
Stilian Cvetkov <49399727+sbcvetkov@users.noreply.github.com>
2021-01-05 16:51:29 +0200

    Update test_total_sum_donator.sql

--- functionality/tests/test_total_sum_donator.sql
+++ functionality/tests/test_total_sum_donator.sql
@@ -3,7 +3,7 @@ DECLARE
     l_donator_id number := 2;
     l_total_sum number;
 BEGIN
-    l_total_sum := f_total_sum_donator(l_donator_id);
+    l_total_sum := people_ops.f_total_sum_donator(l_donator_id);
     IF l_total_sum IS NOT NULL 
     THEN
         dbms_output.put_line('Total sum donated by donator with ID of ' || l_donator_id || ' is $'|| l_total_sum);
@@ -15,7 +15,7 @@ DECLARE
     l_donator_id number := 1232545342;
     l_total_sum number;
 BEGIN
-    l_total_sum := f_total_sum_donator(l_donator_id);
+    l_total_sum := people_ops.f_total_sum_donator(l_donator_id);
     IF l_total_sum IS NOT NULL 
     THEN
         dbms_output.put_line('Total sum donated by donator with ID of ' || l_donator_id || ' is $'|| l_total_sum);
