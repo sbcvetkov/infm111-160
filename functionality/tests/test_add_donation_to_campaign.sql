b582b9bb51b77523e0df65b85a8310eb392b7a44
Stilian Cvetkov <49399727+sbcvetkov@users.noreply.github.com>
2021-01-05 16:45:45 +0200

    Update test_add_donation_to_campaign.sql

--- functionality/tests/test_add_donation_to_campaign.sql
+++ functionality/tests/test_add_donation_to_campaign.sql
@@ -1,27 +1,28 @@
--- test regular case - submitting a donation that doesn't reach target_sum
-insert into donations (
-    donator_id,
-    campaign_id,
-    donation_sum,
-    donation_date
-) values (
-    1,
-    1,
-    1,
-    sysdate
-);
-/
+BEGIN
+    -- test regular case - submitting a donation that doesn't reach target_sum
+    insert into donations (
+        donator_id,
+        campaign_id,
+        donation_sum,
+        donation_date
+    ) values (
+        1,
+        1,
+        1,
+        sysdate
+    );
 
--- test edge case - submitting a donation that will collect or will result in higher sum than target_sum (if target_sum = 100000, current_sum = 300)
-insert into donations (
-    donator_id,
-    campaign_id,
-    donation_sum,
-    donation_date
-) values (
-    1,
-    1,
-    99701,
-    sysdate
-);
-\
+
+    -- test edge case - submitting a donation that will collect or will result in higher sum than target_sum (if target_sum = 100000, current_sum = 300)
+    insert into donations (
+        donator_id,
+        campaign_id,
+        donation_sum,
+        donation_date
+    ) values (
+        1,
+        1,
+        99701,
+        sysdate
+    );
+END;
