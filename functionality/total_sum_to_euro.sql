6e3152aeeac2682618b9139c02408676e0dc94a3
Stilian Cvetkov <49399727+sbcvetkov@users.noreply.github.com>
2021-01-05 12:47:16 +0200

    Update total_sum_to_euro.sql

--- functionality/total_sum_to_euro.sql
+++ functionality/total_sum_to_euro.sql
@@ -1,31 +1,31 @@
-CREATE OR REPLACE function f_total_sum_to_euro
+CREATE OR REPLACE function f_current_sum_to_euro
     ( v_campaign_id IN number )
     RETURN number
 IS
-    l_total_sum number;
-    l_total_sum_euro number;
+    l_current_sum number;
+    l_current_sum_euro number;
     CAMPAIGN_NOT_EXIST exception;
-    CURSOR c_donations
+    CURSOR c_campaign
     IS
-        select count(1)
-        from donations
-        where donator_id = v_campaign_id;
+        select count(*)
+        from campaigns
+        where campaign_id = v_campaign_id;
     n_count number;
 BEGIN
-    open c_donations;
-    fetch c_donations into n_count;
-    close c_donations;
+    open c_campaign;
+    fetch c_campaign into n_count;
+    close c_campaign;
 
     if n_count > 0 then
-        select sum(donation_sum) into l_total_sum from donations where donator_id = v_campaign_id;
-        l_total_sum_euro := l_total_sum * 0.823003;
-        dbms_output.put_line('Campaign with ID of '|| v_campaign_id ||' has collected a sum of €'|| l_total_sum_euro ||'.');
-        RETURN l_total_sum;
+        select current_sum into l_current_sum from campaigns where campaign_id = v_campaign_id;
+        l_current_sum_euro := l_current_sum * 0.823003;
+        RETURN l_current_sum_euro;
     else
         raise CAMPAIGN_NOT_EXIST;
     end if;
     EXCEPTION
     when CAMPAIGN_NOT_EXIST then
         dbms_output.put_line('Donations for campaign with id of '|| v_campaign_id ||' do not exist.');
-END f_total_sum_to_euro;
+        return null;
+END f_current_sum_to_euro;
 /
