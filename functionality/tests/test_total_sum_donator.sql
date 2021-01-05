761e436f66d8ae3b44c452bd6f6190d92e7edf01
Stilian Cvetkov <49399727+sbcvetkov@users.noreply.github.com>
2021-01-05 12:40:28 +0200

    Create test_total_sum_donator.sql

---
+++ functionality/tests/test_total_sum_donator.sql
@@ -0,0 +1,23 @@
+-- get existing donator
+DECLARE
+    l_donator_id number := 2;
+    l_total_sum number;
+BEGIN
+    l_total_sum := f_total_sum_donator(l_donator_id);
+    IF l_total_sum IS NOT NULL 
+    THEN
+        dbms_output.put_line('Total sum donated by donator with ID of ' || l_donator_id || ' is $'|| l_total_sum);
+    END IF;
+END;
+
+-- get non existing donator
+DECLARE
+    l_donator_id number := 1232545342;
+    l_total_sum number;
+BEGIN
+    l_total_sum := f_total_sum_donator(l_donator_id);
+    IF l_total_sum IS NOT NULL 
+    THEN
+        dbms_output.put_line('Total sum donated by donator with ID of ' || l_donator_id || ' is $'|| l_total_sum);
+    END IF;
+END;
