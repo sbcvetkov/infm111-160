567876940ba4317af566a230ecd12e6a13f091c1
Stilian Cvetkov <49399727+sbcvetkov@users.noreply.github.com>
2021-01-05 16:50:52 +0200

    Update test_sum_left_campaign.sql

--- functionality/tests/test_sum_left_campaign.sql
+++ functionality/tests/test_sum_left_campaign.sql
@@ -3,7 +3,7 @@ DECLARE
     l_campaign_id number := 2;
     l_sum_left number;
 BEGIN
-    l_sum_left := f_sum_left_campaign(l_campaign_id);
+    l_sum_left := campaign_ops.f_sum_left_campaign(l_campaign_id);
     IF l_sum_left IS NOT NULL 
     THEN
         dbms_output.put_line('Total sum for campaign with ID of ' || l_campaign_id || ' is $'|| l_sum_left);
@@ -15,7 +15,7 @@ DECLARE
     l_campaign_id number := 231235245;
     l_sum_left number;
 BEGIN
-    l_sum_left := f_sum_left_campaign(l_campaign_id);
+    l_sum_left := campaign_ops.f_sum_left_campaign(l_campaign_id);
     IF l_sum_left IS NOT NULL 
     THEN
         dbms_output.put_line('Total sum for campaign with ID of ' || l_campaign_id || ' is $'|| l_sum_left);
