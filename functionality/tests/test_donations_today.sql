37569c1120df45381b4e9fbc23274ce64d789f97
Stilian Cvetkov <49399727+sbcvetkov@users.noreply.github.com>
2021-01-05 11:44:59 +0200

    Create test_donations_today.sql

---
+++ functionality/tests/test_donations_today.sql
@@ -0,0 +1,40 @@
+-- insert donations and get number of donations for the day
+BEGIN
+    insert into donations (
+        campaign_id,
+        donator_id,
+        donation_sum,
+        donation_date
+    ) values (
+        2,
+        1,
+        30,
+        sysdate
+    );
+
+    insert into donations (
+        campaign_id,
+        donator_id,
+        donation_sum,
+        donation_date
+    ) values (
+        2,
+        2,
+        300,
+        sysdate
+    );
+
+    insert into donations (
+        campaign_id,
+        donator_id,
+        donation_sum,
+        donation_date
+    ) values (
+        3,
+        3,
+        3000,
+        sysdate
+    );
+
+  dbms_output.put_line('Total number of donations for today -> ' || f_donations_today );
+END;
