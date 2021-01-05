09adc2339cea82e795e485848b53c22f1a27135f
Stilian Cvetkov <49399727+sbcvetkov@users.noreply.github.com>
2021-01-05 12:37:18 +0200

    Create test_sum_left_campaign.sql

---
+++ functionality/tests/test_sum_left_campaign.sql
@@ -0,0 +1,23 @@
+-- call existing campaign
+DECLARE
+    l_campaign_id number := 2;
+    l_sum_left number;
+BEGIN
+    l_sum_left := f_sum_left_campaign(l_campaign_id);
+    IF l_sum_left IS NOT NULL 
+    THEN
+        dbms_output.put_line('Total sum for campaign with ID of ' || l_campaign_id || ' is $'|| f_sum_left_campaign(l_campaign_id));
+    END IF;
+END;
+
+-- call non existing campaign
+DECLARE
+    l_campaign_id number := 231235245;
+    l_sum_left number;
+BEGIN
+    l_sum_left := f_sum_left_campaign(l_campaign_id);
+    IF l_sum_left IS NOT NULL 
+    THEN
+        dbms_output.put_line('Total sum for campaign with ID of ' || l_campaign_id || ' is $'|| f_sum_left_campaign(l_campaign_id));
+    END IF;
+END;
