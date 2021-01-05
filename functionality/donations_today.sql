c8296c88f124f0a8e72f6e1592e25be03ef93f2d
Stilian Cvetkov <scvetkov@coursera.org>
2021-01-05 16:53:43 +0200

    rm(functions): removing old functions in favour of packages

--- functionality/donations_today.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE Function f_donations_today
-   RETURN number
-IS
-    l_today_donations number;
-BEGIN
-    select count(*) into l_today_donations from donations where trunc(donation_date) = trunc(sysdate);
-RETURN l_today_donations;
-END f_donations_today;
-/
--- functionality/set_expiration.sql
+++
@@ -1,9 +0,0 @@
--- check for dates and set EXPIRED status if needed
-CREATE OR REPLACE procedure p_set_expired_status
-IS
-BEGIN
-    UPDATE campaigns
-        SET EXPIRY_STATUS = 'EXPIRED'
-        WHERE EXPIRY_DATE < trunc(sysdate);
-END p_set_expired_status;
-/
