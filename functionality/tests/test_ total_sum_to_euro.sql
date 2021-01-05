d760ab2095345d991798eb28f2a5f448b96eef9a
Stilian Cvetkov <49399727+sbcvetkov@users.noreply.github.com>
2021-01-05 12:45:15 +0200

    Create test_ total_sum_to_euro.sql

---
+++ functionality/tests/test_ total_sum_to_euro.sql
@@ -0,0 +1,23 @@
+-- pass valid campaign_id
+DECLARE
+    l_campaign_id number := 1;
+    l_total_sum number;
+BEGIN
+    l_total_sum := f_current_sum_to_euro(l_campaign_id);
+    IF l_total_sum IS NOT NULL 
+    THEN
+        dbms_output.put_line('Total sum donated to campaign with ID of ' || l_campaign_id || ' is €'|| l_total_sum);
+    END IF;
+END;
+
+-- pass non existing campaign_id
+DECLARE
+    l_campaign_id number := 123534;
+    l_total_sum number;
+BEGIN
+    l_total_sum := f_current_sum_to_euro(l_campaign_id);
+    IF l_total_sum IS NOT NULL 
+    THEN
+        dbms_output.put_line('Total sum donated to campaign with ID of ' || l_campaign_id || ' is €'|| l_total_sum);
+    END IF;
+END;
